ef3a4f5a2587aca79f45bab6a6a3fc04d110c213
Ivy-aminga <amingaivy@gmail.com>
2023-07-31 08:39:34 +0300

    movie-app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/GetMovies/NavBar';
+import GetMovies from './components/GetMovies';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MovieDetails from './components/GetMovies/Details/MovieDetails';
+function App() {
+  return (
+    <div className='main'>
+      <BrowserRouter>
+        <Navbar />
+        <GetMovies />
+        <Routes>
+          <Route path='/' element={<GetMovies />} />
+          <Route path='/movies/:movieId' element={<MovieDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
